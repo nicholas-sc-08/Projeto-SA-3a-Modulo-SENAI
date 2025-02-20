70f9ae2f35b45d98b8d961573828c4e1ad1f348f
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-02-20 13:08:18 -0300

    Refazer Projeto..

--- back_end/script.js
+++
@@ -1,9 +0,0 @@
-const express = require(`express`);
-const cors = require(`cors`);
-const body_parser = require(`body-parser`);
-const porta = 3000;
-const app = express();
-app.use(body_parser.json());
-app.use(cors());
-
-app.listen(porta, () => console.log(`Servidor HTTP rodando na porta ${porta}`));
--- src/router/routes.jsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom"
-import Tela_inicial from "../pages/Tela_inicial";
-
-
-const router = createBrowserRouter([
-   {path: `/`, element: <Tela_inicial/>}
-])
-
-export default router;
