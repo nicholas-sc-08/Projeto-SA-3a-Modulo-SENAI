05c9a704a203752fbb1113b443cbe40f7b0811ee
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-03-06 15:09:59 -0300

    Corrigindo Erros

---
+++ back_end/codigo_de_tableas.sql
@@ -0,0 +1,26 @@
+CREATE TABLE clientes(
+
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	email VARCHAR(200) NOT NULL,
+	senha VARCHAR(12) NOT NULL,
+	telefone VARCHAR(14) NOT NULL,
+	cpf VARCHAR(14) NOT NULL,
+	data_de_nascimento DATE NOT NULL
+	
+);
+
+CREATE TABLE enderecos(
+
+	id SERIAL PRIMARY KEY,
+	cep VARCHAR(9) NOT NULL,
+	bairro VARCHAR(100) NOT NULL,
+	logradouro VARCHAR(100) NOT NULL,
+	estado CHAR(2) NOT NULL,
+	cidade VARCHAR(80) NOT NULL,
+	numero VARCHAR(10) NOT NULL,
+	complemento VARCHAR(200) NOT NULL,
+
+	fk_id INT,
+	FOREIGN KEY (fk_id) REFERENCES clientes(id)
+);
