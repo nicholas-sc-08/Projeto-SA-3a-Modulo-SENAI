e3a6c9eb8c3afc66be2f997019293ba992681fb0
dudawl18 <mwolfluiz123a3@gmail.com>
2025-03-13 00:39:40 -0300

    Footer sendo produzido

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,38 @@ function Footer() {
   return (
 
     <div className="footer-content">
-      
-      <h1>aa</h1>
-
+      <div className="conteudo-geral-meio">
+        <div className="parte-de-cima-content">
+          <div className="logo-mais-descricao-content">
+            <h3>Fly</h3>
+            <p>Roupas com história, estilo com propósito. Encontre seu próximo garimpo!</p>
+          </div>
+          <div className="menu-content">
+            <h3>Menu</h3>
+            <p>Início</p>
+            <p>Explorar brechós</p>
+            <p>Doações</p>
+            <p>Buscar</p>
+          </div>
+          <div className="ajuda-content">
+            <h3>Ajuda</h3>
+            <p>Quem somos nós?</p>
+            <p>Contato</p>
+            <p>Termos & Condições </p>
+            <p>Política de Privacidade</p>
+          </div>
+          <div className="FAQ-content">
+            <h3>FAQ</h3>
+            <p>Vender no Fly</p>
+            <p>Como doar roupas</p>
+            <p>Comprar</p>
+            <p>Pagamentos</p>
+          </div>
+        </div>
+        <div className="parte-de-baixo-content">
+           
+        </div>
+      </div>
     </div>
   )
 }
