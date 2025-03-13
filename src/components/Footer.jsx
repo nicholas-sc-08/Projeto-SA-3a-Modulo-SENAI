eed89be5a7bf3d3f5623e07447207b91e269abd4
dudawl18 <mwolfluiz123a3@gmail.com>
2025-03-13 17:22:10 -0300

    continuando o footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './Footer.css'
 
 function Footer() {
@@ -8,33 +9,38 @@ function Footer() {
       <div className="conteudo-geral-meio">
         <div className="parte-de-cima-content">
           <div className="logo-mais-descricao-content">
-            <h3>Fly</h3>
-            <p>Roupas com história, estilo com propósito. Encontre seu próximo garimpo!</p>
+            <div className="logo-e-titulo">
+              <img className='logo-footer' src="./public/img/logo-verdeCamadinha2.svg" alt="" />
+              <h3>FLY</h3>
+            </div>
+            <div className="descricao-footer">
+              <p>Roupas com história, estilo com propósito. Encontre seu próximo garimpo!</p>
+            </div>
           </div>
           <div className="menu-content">
             <h3>Menu</h3>
-            <p>Início</p>
-            <p>Explorar brechós</p>
-            <p>Doações</p>
-            <p>Buscar</p>
+            <Link>Início</Link>
+            <Link>Explorar brechós</Link>
+            <Link>Doações</Link>
+            <Link>Buscar</Link>
           </div>
           <div className="ajuda-content">
             <h3>Ajuda</h3>
-            <p>Quem somos nós?</p>
-            <p>Contato</p>
-            <p>Termos & Condições </p>
-            <p>Política de Privacidade</p>
+            <Link>Quem somos nós?</Link>
+            <Link>Contato</Link>
+            <Link>Termos & Condições </Link>
+            <Link>Política de Privacidade</Link>
           </div>
           <div className="FAQ-content">
             <h3>FAQ</h3>
-            <p>Vender no Fly</p>
-            <p>Como doar roupas</p>
-            <p>Comprar</p>
-            <p>Pagamentos</p>
+            <Link>Vender no Fly</Link>
+            <Link>Como doar roupas</Link>
+            <Link>Comprar</Link>
+            <Link>Pagamentos</Link>
           </div>
         </div>
         <div className="parte-de-baixo-content">
-           
+
         </div>
       </div>
     </div>
