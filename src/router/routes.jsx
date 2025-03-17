e0203b4b50abe39ea88aa6d71a9e859977728983
rinmarys <rinmarysmonagas@gmail.com>
2025-03-17 15:07:29 -0300

    Merge

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -3,7 +3,6 @@ import Tela_incial from "../pages/Tela_incial";
 import Cadastro_cliente from "../pages/Cadastro/Cadastro_cliente";
 import Login from "../pages/Login/Login";
 import DashBoard from "../pages/DashBoard/DashBoard";
-import HeaderBrecho from "../components/HeaderBrecho";
 
 const router = createBrowserRouter([
    {path: `/`, element: <Tela_incial/>},
