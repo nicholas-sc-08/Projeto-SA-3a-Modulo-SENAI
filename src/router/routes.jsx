bdd5482f70cca6e0c69621689bfd61b6941d1d91
dudawl18 <mwolfluiz123a3@gmail.com>
2025-03-19 12:40:36 -0300

    criação da tela perfil brechó

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -10,6 +10,7 @@ import Footer from "../components/Footer";
 import GestaoEstoque from "../pages/Gestao_Estoque/Gestao_Estoque";
 import Cadastro_Produto from "../pages/Cadastro_Produto/Cadastro_Produto";
 import Cadastro_brecho from "../pages/Cadastro/Cadastro_brecho";
+import Perfil_Brecho from "../pages/Perfil_Brecho/Perfil_Brecho";
 
 
 const router = createBrowserRouter([
@@ -22,7 +23,8 @@ const router = createBrowserRouter([
 
    { path: `/footer`, element: <Footer /> },
    { path: `/gestao_estoque`, element: <GestaoEstoque /> },
-   { path: `/cadastro_produto`, element: <Cadastro_Produto /> }
+   { path: `/cadastro_produto`, element: <Cadastro_Produto /> },
+   { path: `/perfil_brecho`, element: <Perfil_Brecho />}
 
 ])
 
