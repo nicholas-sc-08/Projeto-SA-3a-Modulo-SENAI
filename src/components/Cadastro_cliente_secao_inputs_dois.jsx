0024f6701b170c5d9d12de336b0da4aa5326d73a
dudawl18 <mwolfluiz123a3@gmail.com>
2025-03-19 17:27:42 -0300

    footer mobile terminado

--- src/components/Cadastro_cliente_secao_inputs_dois.jsx
+++ src/components/Cadastro_cliente_secao_inputs_dois.jsx
@@ -11,7 +11,7 @@ function Cadastro_cliente_secao_inputs_dois() {
     <div className='container_inputs_secao_um_e_dois'>
 
         <label>Número de Telefone<span>*</span></label>
-        <input type="text" placeholder='(DDD) 99123-4567' required value={form_de_cadastro_cliente.telefone} onChange={e => set_form_de_cadastro_cliente({...form_de_cadastro_cliente, telefone: e.target.value})}/>
+        <input type="text" placeholder='(DD) 99123-4567' required value={form_de_cadastro_cliente.telefone} onChange={e => set_form_de_cadastro_cliente({...form_de_cadastro_cliente, telefone: e.target.value})}/>
 
         <label>CPF<span>*</span></label>
         <input type="text" placeholder='000.000.000-00' value={form_de_cadastro_cliente.cpf} onChange={e => set_form_de_cadastro_cliente({...form_de_cadastro_cliente, cpf: e.target.value})}/>
