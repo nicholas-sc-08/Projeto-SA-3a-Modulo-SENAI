81337cf6670bb20e779aafe94a33f795bee99dca
MayaraStorl <mayarastorl@gmail.com>
2025-03-25 17:18:54 -0300

    fazendo a tela de contato

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -11,12 +11,27 @@ export default function Contato() {
         <p className='p-bege'>Início </p><p>/</p><p> Contato</p>
         <div className='info-contato'>
             <div className='info-para-contato'>
+                <div className='cabecario-ft'>
                 <div className='telefoneCirculo'><i class="bi bi-telephone"></i></div>
+                <h3>Ligue para nós</h3>
+                </div>
+
+                <p>Estamos disponíveis 24 horas por dia, 7 dias por semana.</p>
+                <h4>Telefone: +55 (48) 9999-9999</h4>
+                <div className='linha-contato'></div>
+
+                <div className='cabecario-ft'>
+                <div className='telefoneCirculo'><i class="bi bi-telephone"></i></div>
+                <h3>Escreva para nós</h3>
+                </div>
+
+                <p>Preencha nosso formulário e entraremos em contato com você em até 24 horas.</p>
+                <h4>Emails: customer@exclusive.com</h4>
             </div>
 
         </div>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
