c1937f100dc64867ccc97f84b93e41792d3953b2
rinmarys <rinmarysmonagas@gmail.com>
2025-04-14 15:58:38 -0300

    Atuzalições da tela de inicio

--- src/pages/Tela_incial.jsx
+++ src/pages/Tela_incial.jsx
@@ -40,33 +40,21 @@ function Tela_incial() {
 
       <HeaderUsuario />
 
-      <div className="home-page-container-todas-as-secoes">
-
-        <div className="home-page-secao-um-container">
-          <div className="alinhamento-imagem-texto-secao-um-home-page">
-            <div className="alinhamento-texto-button-secao-um-home-page">
-              <h1>ENCONTRE ROUPAS QUE COMBINAM COM  SEU ESTILO</h1>
-              <p>Explore nossa seleção exclusiva de roupas em brechós cuidadosamente curados, onde cada peça reflete personalidade e estilo único. Encontre itens que combinam com você e expressam sua individualidade de forma autêntica.</p>
-
-              <button>Compre Já</button>
-            </div>
-
-            <div className="alinhamento-imagem-estrelas-secao-um-container">
-
-              <img src="./img/estrelaGrande.png" alt="Estrela verde grande" className='imagem-estrela-verde-grande'/>
-
-              <div className='container-imagem-com-degrade'>
-                <img className='imagem-roupas-um-home-page' src="./img/imagens_telaInicial/ImagemRoupaUm.svg" alt="Imagem roupa um" />
-              </div>
-
-              <img src="./img/estrelaMenor.png" alt="Estrela amarela menor" className='imagem-estrela-amarela-menor' />
-
-            </div>
-          </div>
-
-          <div className="line-verde-grande-home-page"></div>
+      <div className="home-page-secao-um-container">
+        <div className="secao-um-texto-container">
+          <h1>ENCONTRE ROUPAS QUE COMBINAM COM SEU ESTILO</h1>
+          <p>Explore nossa seleção exclusiva de roupas em brechós cuidadosamente curados, onde cada peça reflete personalidade e estilo único. Encontre itens que combinam com você e expressam sua individualidade de forma autêntica.</p>
+          <button>Compre Já</button>
         </div>
 
+        <div className="container-imagem-roupas-numero-um">
+        </div>
+
+        <img className='estrela-verde-home-page' src="img/Estrela_dois_cadastro.svg" alt="estrela verde grande" />
+        <img className='estrela-amarela-home-page' src="img/Estrela_um_cadastro.svg" alt="estrela amarela pequena" />
+      </div>
+
+      <div className="line-home-page-secao-um">
       </div>
 
       <Footer />
