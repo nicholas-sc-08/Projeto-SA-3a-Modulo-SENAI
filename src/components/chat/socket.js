a13fc167412ab5ed7a98aef4fe84647b9423a837
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-04-23 21:43:04 -0300

    ajuste na inicialização de servidor do backend

--- src/components/chat/socket.js
+++ src/components/chat/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // esse aquivo vai faze conexão com o servidor backend
-const socket = io('http://localhost:3000', {
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
   transports: ['websocket'],
   autoConnect: false,
 });
