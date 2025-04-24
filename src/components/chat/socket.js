e26c6b4cd1d85c29b73d52bfac3623ba3913d7fe
rinmarys <rinmarysmonagas@gmail.com>
2025-04-24 14:39:31 -0300

    merge

--- src/components/chat/socket.js
+++ src/components/chat/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // esse aquivo vai faze conexão com o servidor backend
-const socket = io(import.meta.env.VITE_BACKEND_URL, {
+const socket = io('http:// 10.28.144.40:3000/', {
   transports: ['websocket'],
   autoConnect: false,
 });
