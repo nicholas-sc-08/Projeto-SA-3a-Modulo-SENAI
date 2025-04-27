08ee2e749f746b87cf6603c9a03bef7c55301ed7
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-04-27 16:42:47 -0300

    inserindo a api do ngrok para testar com outro computador

--- src/components/chat/socket.js
+++ src/components/chat/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io(`http://localhost:3000`, {
+const socket = io(`https://e4b5-179-89-210-29.ngrok-free.app `, {
   transports: ['websocket'],
   autoConnect: false,
 });
