d72b7c2e31ad23bd7ca1f6e1043328b444f89f68
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-05-10 13:11:53 -0300

    nova url ngrok

--- src/components/chat/socket.js
+++ src/components/chat/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const socket = io(`https://112b-189-8-202-5.ngrok-free.app/`, {
+const socket = io(`https://36d0-179-93-239-81.ngrok-free.app`, {
   transports: ['websocket'],
   extraHeaders: { 'ngrok-skip-browser-warning': 'true' }
 });
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://112b-189-8-202-5.ngrok-free.app/', headers: { "ngrok-skip-browser-warning": "true", "Content-Type": "application/json" }});
+const api = axios.create({ baseURL: 'https://36d0-179-93-239-81.ngrok-free.app', headers: { "ngrok-skip-browser-warning": "true", "Content-Type": "application/json" }});
 
 export default api;
