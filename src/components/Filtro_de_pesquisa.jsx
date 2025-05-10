2156344759e98763b6c4261e317e6e34c93bc0af
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-05-10 18:29:56 -0300

    Fazendo filtro de pesquisa

--- src/components/Filtro_de_pesquisa.jsx
+++ src/components/Filtro_de_pesquisa.jsx
@@ -1,240 +1,119 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Filtro_de_pesquisa.css';
+import { useContext } from 'react';
+import { GlobalContext } from '../contexts/GlobalContext';
+import api from '../services/api';
 
 function Filtro_de_pesquisa() {
-    const [categorias_visiveis, set_categorias_visiveis] = useState({
-        preco: true,
-        tamanho: false,
-        estilos: false,
-        tshirts: false,
-        shorts: false,
-        jeans: false,
-        casual: false,
-        formal: false,
-        festa: false,
-        academia: false,
-    });
 
-    const [preco, setPreco] = useState(1000);
+    const { array_categorias, set_array_categorias } = useContext(GlobalContext);
+    const [ botao_titulo_precos_deg, set_botao_titulo_precos_deg ] = useState(`rotate(0deg)`);
+    const [ preco_filtro_de_pesquisa, set_preco_filtro_de_pesquisa ] = useState(`25`);
+    const [ exibir_filtro_do_preco, set_exibir_filtro_do_preco ] = useState(false);
+    const [ altura_preco_filtro_de_pesquisa, set_altura_preco_filtro_de_pesquisa ] = useState(`0%`);
 
-    const handlePrecoChange = (event) => {
-        setPreco(event.target.value);
+    useEffect(() => {
+
+        buscar_categorias();
+
+    }, []);
+
+    async function buscar_categorias(){
+
+        try {
+
+            const categorias = await api.get(`/categorias`);
+            set_array_categorias(categorias.data);
+
+        } catch (erro) {
+          
+            console.error(erro);
+        };
     };
 
-    function exibir_opcoes_de_categoria(categoria){
+    function girar_botao_titulo_preco(){
 
-        if(categoria == 'tshirts'){
+        botao_titulo_precos_deg == `rotate(0deg)` ? set_botao_titulo_precos_deg(`rotate(180deg)`) : set_botao_titulo_precos_deg(`rotate(0deg)`);
+        set_exibir_filtro_do_preco(!exibir_filtro_do_preco); 
+        
+    };
 
-            
+    function categorias_principais(categoria){
+
+        const index_da_cateogria = array_categorias.findIndex(categoria_principal => categoria_principal.nome.toUpperCase() == categoria.toUpperCase() && categoria_principal.sub_categoria == false);
+
+        if(index_da_cateogria != -1){
+
+            return array_categorias[index_da_cateogria].nome;
         };
     };
 
     return (
-        <aside className="filtro-container">
-            <div className="alinhamento-titulo-filtro-pesquisa">
-                <div className="titulo-imagem-filtro-pesquisa">
-                    <h1>Filtros</h1>
-                    <img src="./img/icons/icone-filtro-pesquisa.svg" alt="" />
+        <div className="container_filtro_de_pesquisa">
+
+            <div className="container_titulo_filtro_de_pesquisa">
+
+                <h1>Filtros</h1>
+                <img src="./img/icons/icone-filtro-pesquisa.svg" alt="" />
+           
+            </div>
+            
+            <div className="container_pesquisa_por_categoria">
+
+                <div className="container_pesquisa_filtro_um">
+
+                    <p>{categorias_principais(`camiseta`)}</p>
+                    <img src="./img/icons/seta_do_filtro_de_pesquisa.svg" alt="" />
+                
                 </div>
-                <div className="line-filtro-pesquisa"></div>
+
+                <div className="container_pesquisa_filtro_dois">
+
+                    <p>{categorias_principais(`shorts`)}</p>
+                    <img src="./img/icons/seta_do_filtro_de_pesquisa.svg" alt="" />
+                
+                </div>
+
+                <div className="container_pesquisa_filtro_tres">
+
+                    <p>{categorias_principais(`jeans`)}</p>
+                    <img src="./img/icons/seta_do_filtro_de_pesquisa.svg" alt="" />
+                
+                </div>    
+            
             </div>
 
-            {/* T-Shirts */}
-            <div className="filtro-categorias-roupas">
-                <h4 onClick={() => exibir_opcoes_de_categoria('tshirts')}>
-                    T-shirts
-                    <svg
-                        className={`flecha-icon ${categorias_visiveis.tshirts ? 'flecha-open' : ''}`}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M9 6l6 6-6 6" />
-                    </svg>
-                </h4>
 
-                {categorias_visiveis.tshirts && (
-                    <ul className="categorias-ocultas">
-                        <li><button>Camisetas de manga longa</button></li>
-                        <li><button>Camisetas de algodão</button></li>
-                        <li><button>Camisetas de manga curta</button></li>
-                    </ul>
-                )}
+            <div className="container_preco_titulo_filtro_de_pesquisa">
+
+                <h1>Preços</h1>
+                <button onClick={() => girar_botao_titulo_preco()}><img src="./img/icons/seta_do_filtro_de_pesquisa_titulo.svg" alt="" style={{transform: botao_titulo_precos_deg}}/></button>
+           
             </div>
 
-            {/* Shorts */}
-            <div className="filtro-categorias-roupas">
-                <h4 onClick={() => exibir_opcoes_de_categoria('shorts')}>Shorts
-                    <svg
-                        className={`flecha-icon ${categorias_visiveis.shorts ? 'flecha-open' : ''}`}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M9 6l6 6-6 6" />
-                    </svg>
-                </h4>
-                {categorias_visiveis.shorts && (
-                    <ul className="categorias-ocultas">
-                        <li><button>Shorts de praia</button></li>
-                        <li><button>Shorts de algodão</button></li>
-                        <li><button>Shorts jeans</button></li>
-                    </ul>
-                )}
+            <div className="container_preco_filtro_de_pesquisa">
+
+            { exibir_filtro_do_preco &&
+
+            <div className="container_selecionar_preco_filtro_de_pesquisa">
+
+                <input type="range" min={0} max={200} step={1} value={preco_filtro_de_pesquisa} onChange={e => set_preco_filtro_de_pesquisa(e.target.value)}/>
+                
+                <div className="container_exibir_preco_filtro_de_pesquisa">
+
+                    <p>R$ 0,00</p>
+                    <p>R$ {preco_filtro_de_pesquisa},00</p>
+
+                </div>
+
             </div>
 
-            {/* Jeans */}
-            <div className="filtro-categorias-roupas">
-                <h4 onClick={() => exibir_opcoes_de_categoria('jeans')}>Jeans
-                    <svg
-                        className={`flecha-icon ${categorias_visiveis.jeans ? 'flecha-open' : ''}`}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M9 6l6 6-6 6" />
-                    </svg>
-                </h4>
-                {categorias_visiveis.jeans && (
-                    <ul className="categorias-ocultas">
-                        <li><button>Jeans slim</button></li>
-                        <li><button>Jeans reto</button></li>
-                        <li><button>Jeans skinny</button></li>
-                    </ul>
-                )}
+            }
+
             </div>
 
-            {/* Preços */}
-            <div className="filtro-pesquisa-preco">
-                <h4 onClick={() => exibir_opcoes_de_categoria('preco')}>Preços
-                    <svg
-                        className={`flecha-icon ${categorias_visiveis.preco ? 'flecha-open' : ''}`}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M9 6l6 6-6 6" />
-                    </svg>
-                </h4>
-                {categorias_visiveis.preco && (
-                    <div className='container-input-preco-filtro'>
-                        <input
-                            type="range"
-                            min="0"
-                            max="200"
-                            step="1"
-                            value={preco}
-                            onChange={handlePrecoChange}
-                            className='input-preco-filtro'
-                        />
-                        <div className="valores-preco-filtro">R$ 0,00 - R$ {preco},00</div>
-                    </div>
-                )}
-            </div>
 
-            {/* Tamanho */}
-            <div className="filtro-tamanho">
-                <h4 onClick={() => exibir_opcoes_de_categoria('tamanho')}>Tamanho
-                    <svg
-                        className={`flecha-icon ${categorias_visiveis.tamanho ? 'flecha-open' : ''}`}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M9 6l6 6-6 6" />
-                    </svg>
-                </h4>
-                {categorias_visiveis.tamanho && (
-                    <div className="opcoes-tamanho-filtro">
-                        <button>PP</button>
-                        <button>P</button>
-                        <button>M</button>
-                        <button>G</button>
-                        <button>GG</button>
-                        <button>XG</button>
-                    </div>
-                )}
-            </div>
-
-            {/* Estilos */}
-            <div className="filtro-estilos">
-                <h4 onClick={() => exibir_opcoes_de_categoria('estilos')}>Estilos
-                    <svg
-                        className={`flecha-icon ${categorias_visiveis.estilos ? 'flecha-open' : ''}`}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M9 6l6 6-6 6" />
-                    </svg>
-                </h4>
-                {categorias_visiveis.estilos && (
-                    <ul>
-                        <div>
-                            <li><button className='button-estilos-categorias-visiveis' onClick={() => exibir_opcoes_de_categoria('casual')}>Casual</button>
-                                <svg
-                                    className={`flecha-icon ${categorias_visiveis.casual ? 'flecha-open' : ''}`}
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path d="M9 6l6 6-6 6" />
-                                </svg></li>
-                            {categorias_visiveis.casual && (
-                                <ul className="categorias-ocultas">
-                                    <li><button>Roupas casuais</button></li>
-                                    <li><button>Casacos</button></li>
-                                    <li><button>Blusas</button></li>
-                                </ul>
-                            )}
-                        </div>
-
-                        <li><button className='button-estilos-categorias-visiveis' onClick={() => exibir_opcoes_de_categoria('formal')}>Formal</button>
-                            <svg
-                                className={`flecha-icon ${categorias_visiveis.formal ? 'flecha-open' : ''}`}
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M9 6l6 6-6 6" />
-                            </svg></li>
-                        {categorias_visiveis.formal && (
-                            <ul className="categorias-ocultas">
-                                <li><button>Ternos</button></li>
-                                <li><button>Camisas sociais</button></li>
-                                <li><button>Saia lápis</button></li>
-                            </ul>
-                        )}
-
-                        <li><button className='button-estilos-categorias-visiveis' onClick={() => exibir_opcoes_de_categoria('festa')}>Festa</button>
-                            <svg
-                                className={`flecha-icon ${categorias_visiveis.festa ? 'flecha-open' : ''}`}
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M9 6l6 6-6 6" />
-                            </svg></li>
-                        {categorias_visiveis.festa && (
-                            <ul className="categorias-ocultas">
-                                <li><button>Vestidos de festa</button></li>
-                                <li><button>Roupas de gala</button></li>
-                                <li><button>Trajes de noite</button></li>
-                            </ul>
-                        )}
-
-                        <li><button className='button-estilos-categorias-visiveis' onClick={() => exibir_opcoes_de_categoria('academia')}>Academia</button>
-                            <svg
-                                className={`flecha-icon ${categorias_visiveis.academia ? 'flecha-open' : ''}`}
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M9 6l6 6-6 6" />
-                            </svg></li>
-                        {categorias_visiveis.academia && (
-                            <ul className="categorias-ocultas">
-                                <li><button>Leggings</button></li>
-                                <li><button>Blusas de treino</button></li>
-                                <li><button>Shorts de academia</button></li>
-                            </ul>
-                        )}
-                    </ul>
-                )}
-            </div>
-
-            <button className="btn-aplicar">Aplicar Filtro</button>
-        </aside>
+        </div>
     );
 }
 
