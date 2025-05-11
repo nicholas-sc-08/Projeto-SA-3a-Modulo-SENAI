6b2e4c75ca6811d4c70894f0442f77922df78e5f
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-05-11 14:02:43 -0300

    alterando url do ngrok

--- src/components/chat/socket.js
+++ src/components/chat/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const socket = io(`https://36d0-179-93-239-81.ngrok-free.app`, {
+const socket = io(`https://455d-179-89-214-186.ngrok-free.app`, {
   transports: ['websocket'],
   extraHeaders: { 'ngrok-skip-browser-warning': 'true' }
 });
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://36d0-179-93-239-81.ngrok-free.app', headers: { "ngrok-skip-browser-warning": "true", "Content-Type": "application/json" }});
+const api = axios.create({ baseURL: 'https://455d-179-89-214-186.ngrok-free.app', headers: { "ngrok-skip-browser-warning": "true", "Content-Type": "application/json" }});
 
 export default api;
