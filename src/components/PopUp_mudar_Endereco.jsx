e3d8ff1473ffe9832a732b1ca60cc8e6c4806367
dudawl18 <mwolfluiz123a3@gmail.com>
2025-05-12 13:40:25 -0300

    css do pop up endereco brecho finalizado

--- src/components/PopUp_mudar_Endereco.jsx
+++ src/components/PopUp_mudar_Endereco.jsx
@@ -1,12 +1,17 @@
 import '../components/PopUp_mudar_Endereco.css'
+import { useNavigate } from 'react-router-dom'
 
-function PopUp_mudar_Endereco() {
+function PopUp_mudar_Endereco({ fecharPopUp }) {
+  const navegar = useNavigate()
 
   return (
     <div className="tela-inteira-content">
       <div className="divs-centrais-content">
         <div className="voltar-e-menu-content">
-          
+          <img src="./img/icons/Voltar-icone-verde.svg" alt="Voltar" onClick={fecharPopUp} />
+          <img src="./img/icons/Menu-hamburguer-verde-escuro.svg" alt="Menu" />
+
+
         </div>
         <div className="popUp-mudar-endereco-content">
           <div className="titulo-e-subtitulo-content">
@@ -14,29 +19,33 @@ function PopUp_mudar_Endereco() {
             <p className="subtitulo-endereco-brecho-popUp">De onde você deseja enviar os seus produtos?</p>
           </div>
           <div className="inputs-do-popUp-content">
-            <div className="rua-logra-input-content">
-              <p className="topicos-input-endereco">Rua/Logradouro</p>
-              <input type="text" placeholder='Rua do Vasco'/>
-            </div>
-            <div className="numero-input-content">
-              <p className="topicos-input-endereco">Número</p>
-              <input type="text" placeholder='283'/>
+            <div className="juncao-rua-logra-e-numero-content">
+              <div className="rua-logra-input-content">
+                <p className="topicos-input-endereco">Rua/Logradouro</p>
+                <input type="text" placeholder='Rua do Vasco' />
+              </div>
+              <div className="numero-input-content">
+                <p className="topicos-input-endereco">Número</p>
+                <input type="text" placeholder='283' />
+              </div>
             </div>
             <div className="complemento-input-content">
               <p className="topicos-input-endereco">Complemento</p>
-              <input type="text" placeholder='Apartamento 07'/>
+              <input type="text" placeholder='Apartamento 07' />
             </div>
-            <div className="cep-input-content">
-              <p className="topicos-input-endereco">CEP</p>
-              <input type="text" placeholder='88058-080'/>
-            </div>
-            <div className="bairro-input-content">
-              <p className="topicos-input-endereco">Bairro</p>
-              <input type="text" placeholder='Ingleses do Rio Vermelho'/>
+            <div className="juncao-cep-e-bairro-content">
+              <div className="cep-input-content">
+                <p className="topicos-input-endereco">CEP</p>
+                <input type="text" placeholder='88058-080' />
+              </div>
+              <div className="bairro-input-content">
+                <p className="topicos-input-endereco">Bairro</p>
+                <input type="text" placeholder='Ingleses do Rio Vermelho' />
+              </div>
             </div>
             <div className="ponto-de-refe-input-content">
               <p className="topicos-input-endereco">Ponto de Referência</p>
-              <input type="text" placeholder='Brasil Atacadista'/>
+              <input type="text" placeholder='Brasil Atacadista' />
             </div>
           </div>
           <div className="salvar-endereco-content">
--- src/pages/Perfil_Brecho/Edicao_perfil_brecho.jsx
+++ src/pages/Perfil_Brecho/Edicao_perfil_brecho.jsx
@@ -22,7 +22,7 @@ function Edicao_perfil_brecho() {
         <div className="edicao-perfil-brecho-content">
           <div className="parte-esquerda-div-central">
             <div className="perfil-brecho-logo-content">
-            <img src="" alt="" />
+              <img src="" alt="" />
             </div>
             <div className="info-horario-perfil-brecho">
               <textarea name="" className="horario-preenchido-brecho" id="">Exemplo: Segunda à Sexta: 08:00 - 16:00 --- Sábado à Domingo: 10:00 - 17:00</textarea>
@@ -70,10 +70,10 @@ function Edicao_perfil_brecho() {
             </div>
             <div className="endereco-e-senha-infos-content">
               <p className="titulo-do-input">Endereço</p>
-              <button onClick={abrirPopUp}>Clique para alterar</button>
+              <button onClick={() => setMostrarPopUp(true)}>Clique para alterar</button>
               <p className="titulo-do-input">Mudança de Senha</p>
               <input type="text" placeholder="Nova Senha" />
-              <input type="text" placeholder='Confirme sua senha'/>
+              <input type="text" placeholder='Confirme sua senha' />
             </div>
             <div className="botao-editar-content">
               <button>Editar</button>
@@ -81,7 +81,10 @@ function Edicao_perfil_brecho() {
           </div>
         </div>
       </div>
-      {mostrarPopUp && <PopUp_mudar_Endereco onClose={fecharPopUp} />}
+      {mostrarPopUp && (
+        <PopUp_mudar_Endereco fecharPopUp={() => setMostrarPopUp(false)} />
+      )}
+
       <Footer />
     </div>
   )
