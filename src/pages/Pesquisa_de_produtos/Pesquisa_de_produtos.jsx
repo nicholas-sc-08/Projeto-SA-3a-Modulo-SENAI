dd639a1b888a75b33f53803b862378306315308f
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-05-14 15:28:05 -0300

    fazendo a exibição de produtos

--- src/pages/Pesquisa_de_produtos/Pesquisa_de_produtos.jsx
+++ src/pages/Pesquisa_de_produtos/Pesquisa_de_produtos.jsx
@@ -1,16 +1,68 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import './Pesquisa_de_produtos.css'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import Filtro_de_pesquisa from '../../components/Filtro_de_pesquisa'
+import { GlobalContext } from '../../contexts/GlobalContext'
+import api from '../../services/api'
 
 function Pesquisa_de_produtos() {
+
+    const { array_produtos, set_array_produtos } = useContext(GlobalContext);
+
+    useEffect(() => {
+
+        buscar_produtos();
+
+    }, []);
+
+    async function buscar_produtos(){
+
+        try {
+            
+            const produtos = await api.get(`/produtos`);
+            set_array_produtos(produtos.data);
+
+        } catch (erro) {
+          
+            console.error(erro);
+        };
+    };
+
     return (
         <div className='container-alinhamento-all-pages'>
             <Header tipo='usuario' />
 
-            <div className="all-page-informacoes-alinhamento">
-                <Filtro_de_pesquisa />
+            <div className="container_conteudo_pesquisa_produtos">
+
+                <div className="all-page-informacoes-alinhamento">
+                    <Filtro_de_pesquisa />
+                </div>
+
+                <div className="container_exibir_produtos">
+
+                    {array_produtos.map((produto, i) => (
+                        
+                        <div key={i} className='container_produto'>
+                            
+                            <div className="container_produto_img">
+
+                                <img src={produto.imagem[0]} alt="aa" />
+                            
+                            </div>
+
+                            <div className="container_produto_info">
+
+                                <h2>{produto.nome}</h2>
+                                <span>R${produto.preco}</span>
+                            
+                            </div>
+
+                        </div>
+                    ))}
+
+                </div>
+
             </div>
 
             <Footer />
