d8f51cca7f6ca7cd0f6064917f16251bb40547ae
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-05-21 15:04:44 -0300

    conserto do erro ao renderizar imagem de perfil do cliente no chat + ao exibir produtos ele exibe a logo de qual brecho ele pertence

--- src/pages/Pesquisa_de_produtos/Pesquisa_de_produtos.jsx
+++ src/pages/Pesquisa_de_produtos/Pesquisa_de_produtos.jsx
@@ -12,17 +12,31 @@ import Chat_conversa from '../../components/chat/Chat_conversa';
 function Pesquisa_de_produtos() {
 
     const { array_produtos, set_array_produtos } = useContext(GlobalContext);
+    const { array_brechos, set_array_brechos } = useContext(GlobalContext);
     const { usuario_logado, set_usuario_logado } = useContext(GlobalContext);
     const { conversa_aberta, set_conversa_aberta } = useContext(GlobalContext);
-
     const { produto, set_produto } = useContext(GlobalContext);
     const navegar_para_produto = useNavigate(null);
 
     useEffect(() => {
 
         buscar_produtos();
+        buscar_brechos();
 
     }, []);
+
+    async function buscar_brechos(){
+
+        try {
+
+            const brechos = await api.get(`/brechos`);
+            set_array_brechos(brechos.data);
+            
+        } catch (erro) {
+          
+            console.error(erro);
+        };
+    };
 
     async function buscar_produtos(){
 
@@ -41,6 +55,24 @@ function Pesquisa_de_produtos() {
 
         set_produto(produto);
         navegar_para_produto(`/produto`);
+    };
+
+    function preco_do_produto(preco){
+
+        const preco_separado = String(preco).split(`.`);
+        const decimal = preco_separado[preco_separado.length -1];
+
+        return decimal < 10 ? `${preco_separado[0]},${decimal}0 ` : preco;
+    };
+
+    function imagem_de_perfil_brecho(_id){
+
+        const encontrar_brecho = array_brechos.find(brecho => brecho._id == _id);
+        
+        if(encontrar_brecho){
+
+            return encontrar_brecho.logo;
+        };
     };
 
     return (
@@ -62,13 +94,19 @@ function Pesquisa_de_produtos() {
                             <div className="container_produto_img">
 
                                 <img src={produto.imagem[0]} alt="aa" />
-                            
+
                             </div>
 
                             <div className="container_produto_info">
 
-                                <h2>{produto.nome}</h2>
-                                <span>R${produto.preco}</span>
+                                <div className='container_produto_titulo'>
+
+                                    <h2>{produto.nome}</h2>
+                                    <img src={imagem_de_perfil_brecho(produto.fk_id_brecho)} alt="" />
+
+                                </div>
+                                
+                                <span>R${preco_do_produto(produto.preco)}</span>
                             
                             </div>
 
