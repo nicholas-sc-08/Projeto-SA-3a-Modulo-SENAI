207a20166f4201ecd57cd597c0502738d3643225
dudawl18 <mwolfluiz123a3@gmail.com>
2025-06-02 14:03:22 -0300

    puxando as informações do brecho para o perfil

--- src/pages/Perfil_Brecho/Perfil_Brecho.jsx
+++ src/pages/Perfil_Brecho/Perfil_Brecho.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+
 import Footer from '../../components/Footer'
 import '../Perfil_Brecho/Perfil_Brecho.css'
 import Header from '../../components/Header'
