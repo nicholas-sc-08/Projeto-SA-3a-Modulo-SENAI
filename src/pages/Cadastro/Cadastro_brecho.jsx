0ec9cbeed6dbba0aa71b86f829bc8eb91fd111ce
rinmarys <rinmarysmonagas@gmail.com>
2025-06-03 21:38:22 -0300

    Teste de animação login e cadastro

--- src/pages/Cadastro/Cadastro_brecho.jsx
+++ src/pages/Cadastro/Cadastro_brecho.jsx
@@ -5,8 +5,8 @@ import { GlobalContext } from '../../contexts/GlobalContext';
 import SecaoInputsUmBrecho from '../../components/CadastroBrechoSecaoInputsUm.jsx'
 import SecaoInputsDoisBrecho from '../../components/CadastroBrechoSecaoInputsDois.jsx'
 import SecaoInputsTresBrecho from '../../components/CadastroBrechoSecaoInputsTres.jsx'
-import axios from 'axios';
 import api from '../../services/api.js';
+import { AnimatePresence, motion } from "framer-motion";
 
 function Cadastro_brecho() {
 
@@ -15,7 +15,7 @@ function Cadastro_brecho() {
   const { cadastroParteDoisBrecho, setCadastroParteDoisBrecho } = useContext(GlobalContext);
   const { cadastroParteTresBrecho, setCadastroParteTresBrecho } = useContext(GlobalContext);
   const { array_brechos, set_array_brechos } = useContext(GlobalContext);
-  const { array_clientes, set_array_clientes } = useContext(GlobalContext); 
+  const { array_clientes, set_array_clientes } = useContext(GlobalContext);
   const { enderecoDoBrecho, setEnderecoDoBrecho } = useContext(GlobalContext);
   const { array_enderecos, set_array_enderecos } = useContext(GlobalContext);
 
@@ -29,6 +29,8 @@ function Cadastro_brecho() {
 
   const diaDeHoje = new Date();
   const navegar_para_outra_tela = useNavigate();
+
+  const [animandoCadastro, setAnimandoCadastro] = useState(false);
 
   /* Para a verificação do input de email -- se possui caracteres antes do @ e se há os dominios "gmail.com" e "hotmail.com" */
   const terminaComGmailOuHotmail = formCadastroBrecho.email.endsWith('@gmail.com') || formCadastroBrecho.email.endsWith('@hotmail.com')
@@ -55,30 +57,30 @@ function Cadastro_brecho() {
     };
   };
 
-  async function informacoes_clientes(){
+  async function informacoes_clientes() {
 
     try {
 
       const clientes = await api.get(`/clientes`);
       set_array_clientes(clientes.data);
 
-      
+
     } catch (erro) {
-      
+
       console.error(erro);
     };
   };
 
-  async function buscar_enderecos(){
+  async function buscar_enderecos() {
 
     try {
-      
+
       const enderecos = await api.get(`/enderecos`);
       set_array_enderecos(enderecos.data);
 
 
     } catch (erro) {
-      
+
       console.error(erro);
     };
   };
@@ -88,21 +90,21 @@ function Cadastro_brecho() {
     e.preventDefault();
 
     try {
-     
+
       const resposta = await api.post(`/brechos`, formCadastroBrecho);
-      
+
       const enderecoDoBrechoComFK = {
-        
+
         ...enderecoDoBrecho,
         id_brecho: resposta.data._id
-      };      
-      
+      };
+
       await api.post(`/enderecos`, enderecoDoBrechoComFK);
-      
+
       informacoesBrecho();
       buscar_enderecos();
       navegar_para_outra_tela(`/login`);
-      
+
     } catch (erro) {
 
       console.error(erro);
@@ -237,14 +239,14 @@ function Cadastro_brecho() {
         };
       };
 
-      for(let i = 0; i < array_clientes.length; i++){
+      for (let i = 0; i < array_clientes.length; i++) {
 
-        if(array_clientes[i].email == formCadastroBrecho.email){
+        if (array_clientes[i].email == formCadastroBrecho.email) {
 
           emailJaCadastrado = true;
         };
 
-        if(array_clientes[i].telefone == formCadastroBrecho.telefone){
+        if (array_clientes[i].telefone == formCadastroBrecho.telefone) {
 
           telefoneJaCadastrado = true;
         };
@@ -272,7 +274,7 @@ function Cadastro_brecho() {
         setMensagemErro(`O email deve conter caracteres antes do @`);
         return
       }
-      
+
 
       switch (true) {
 
@@ -343,10 +345,19 @@ function Cadastro_brecho() {
     };
   }
 
+  const LoginCadastro = () => {
+    setAnimandoCadastro(true); // dispara animação de saída
+
+    // espera 800ms pra redirecionar após a animação
+    setTimeout(() => {
+      navegar_para_outra_tela('/login');
+    }, 600);
+  };
+
   return (
     <div>
       <div className="alinhamento-fases-container-cadastro">
-        <div className="container-ir-para-tela-login-alinhamento">
+        {/* <div className="container-ir-para-tela-login-alinhamento">
           <div className="container-informacoes-login-cadastro-brecho">
 
             <img src="./img/Estrela_um_cadastro.svg" alt="estrela" className='estrela-um-cadastro' />
@@ -357,6 +368,27 @@ function Cadastro_brecho() {
 
             <img src="./img/Estrela_dois_cadastro.svg" alt="estrela" className='estrela-dois-cadastro' />
           </div>
+        </div> */}
+
+        <div className="container-ir-para-tela-login-alinhamento">
+          <AnimatePresence>
+            {!animandoCadastro && (
+              <motion.div
+                className="container-informacoes-login-cadastro-brecho"
+                initial={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 10, x: 755 }} // Se mueve a la derecha
+                transition={{ duration: 0.8, ease: "easeOut" }}
+              >
+                <img className='estrela-um-cadastro' src="./img/estrelaMenor.png" alt="" />
+
+                <h1>Bem-vindo de volta! Sentimos sua falta.</h1>
+                <p>A moda circular nunca para! Entre na sua conta e continue fazendo parte desse movimento incrível.</p>
+                <button onClick={LoginCadastro} type='button'>Entrar</button>
+
+                <img className='estrela-dois-cadastro' src="./img/estrelaGrande.png" alt="" />
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
 
         <div className="alinhamento-elipses-com-container-inputs">
