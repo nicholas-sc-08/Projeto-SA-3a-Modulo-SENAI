fcbae0d3e87222eb316c49c78262159afa925b8d
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-06-05 16:44:54 -0300

    alteração url ngrok

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://aa00-189-8-202-5.ngrok-free.app ', headers: { "ngrok-skip-browser-warning": "true", "Content-Type": "application/json" }});
+const api = axios.create({ baseURL: 'https://f8fe-189-8-202-5.ngrok-free.app', headers: { "ngrok-skip-browser-warning": "true", "Content-Type": "application/json" }});
 
 export default api;
