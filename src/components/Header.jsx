a42e72c5efc2cbf0c632de1a9b01721b4db3d747
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-06-05 15:15:48 -0300

    ajustando a tela de cadastro produto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -94,16 +94,7 @@ function Header({ tipo }) {
                         <img src="/img/icons/IconeSacola.svg" alt="Sacola" />
                     </button>
                     
-                    <AnimatePresence>
-
-                        <motion.div initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -10 }} transition={{ duration: 0.2 }}>
-
-                            {sacola_aberta && <Sacola/>}
-
-                        </motion.div>
-
-                    </AnimatePresence>
-
+                        {sacola_aberta && <Sacola/>}
 
                     {tipo === 'brecho' && (
                         <button className="button-chat-navbar">
