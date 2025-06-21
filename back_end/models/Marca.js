3581464f48f775a17a96c5a01d64f9804181d456
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-06-21 11:43:47 -0300

    texto da descrição do produto antes estava ultrapassando o limite fornecido, ao invés de quebrar de linha. Erro corrigido

--- back_end/models/Marca.js
+++ back_end/models/Marca.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-const EstoqueSchema = new mongoose.Schema({
+const MarcaSchema = new mongoose.Schema({
 
     nome: { type: String, required: true },
 }, { timestamps: true })
 
-const Estoque = mongoose.model("Estoque", EstoqueSchema);
-module.exports = Estoque
+const Marca = mongoose.model("Marca", MarcaSchema);
+module.exports = Marca;
