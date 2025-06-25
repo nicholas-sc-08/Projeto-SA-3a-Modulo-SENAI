e90022fc53c6c6a9d8cd2eb609c85d5e35905464
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-06-25 14:18:44 -0300

    alteração url ngrok

--- src/components/chat/socket.js
+++ src/components/chat/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const socket = io(`https://f616-189-8-202-5.ngrok-free.app`, {
+const socket = io(`https://0d86-189-8-202-5.ngrok-free.app`, {
   transports: ['websocket'],
   extraHeaders: { 'ngrok-skip-browser-warning': 'true' }
 });
