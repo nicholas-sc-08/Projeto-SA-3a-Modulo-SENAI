73280eae34f232639fcadbe9ce19983456da2a8d
GabrielFerLacerda <gabrielferlacerda@gmail.com>
2025-06-28 01:55:08 -0300

    Adiciona script.js limpo sem a chave

--- back_end/script.js
+++ back_end/script.js
@@ -1,5 +1,6 @@
 const express = require(`express`);
 const cors = require(`cors`);
+require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const http = require(`http`);
 const app = express();
